de6f482c8a9087e5e66ca5ff88e70eff2ba3addf
lyubich <anastasia.lyubich@gmail.ru>
2015-07-09 14:09:13 +0600

    The basis of the project

---
+++ src/main/java/it/sevenbits/graphicartsindustry/web/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package it.sevenbits.graphicartsindustry.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String index() {
+        // Так как нет аннотации @ResponseBody, то spring будет искать шаблон по адресу home/index
+        // Если шаблона не будет найдено, то вернется 404 ошибка
+        return "home/index";
+    }
+}

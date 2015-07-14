e392f7c83f4f581e504f1e8c91a6fcad68b8a586
sadullaeva <sadullaeva.marina@gmail.com>
2015-07-14 18:12:56 +0600

    Add configuration for production tomcat deploying

--- src/main/java/it/sevenbits/graphicartsindustry/Application.java
+++ src/main/java/it/sevenbits/graphicartsindustry/Application.java
@@ -2,12 +2,19 @@ package it.sevenbits.graphicartsindustry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
         app.setShowBanner(false);
         app.run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }

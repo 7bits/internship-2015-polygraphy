262eb4820a2f31e98471e79ce526eb00f95d9581
lyubich <anastasia.lyubich@gmail.ru>
2015-07-17 15:10:20 +0600

    Fixed seach: origin or Origin

---
+++ src/main/java/it/sevenbits/graphicartsindustry/core/repository/PolygraphyProvider.java
@@ -0,0 +1,13 @@
+package it.sevenbits.graphicartsindustry.core.repository;
+
+import java.util.Map;
+
+public class PolygraphyProvider {
+
+    public static String findLikePolygraphies(final Map params) {
+
+        String result = "SELECT id, name, addres, phone FROM polygraphy AS p LEFT JOIN contacts AS c " +
+        "ON p.id=c.polygraphy_id WHERE name ILIKE '%"+params.get("query")+"%'";
+        return result;
+    }
+}

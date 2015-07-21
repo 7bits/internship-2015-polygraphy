2e156d1a8718b6113d5220281a00b25eb9cef01d
sadullaeva <sadullaeva.marina@gmail.com>
2015-07-21 23:53:19 +0600

    Removed redraw checkboxes. Fixed minor bugs.

--- src/main/resources/public/scripts/app.js
+++ src/main/resources/public/scripts/app.js
@@ -2,8 +2,20 @@
   console.log("Hello world");
 })(jQuery);*/
 
-(function($, undefined){
+/*(function($, undefined){
     $(function(){
         $("p").css("color", "#333");
     });
-})(jQuery);
+})(jQuery);*/
+
+/*//кликаешь на submit и форма пытается свернуться
+$(function(){
+    $('button').click(function(){
+        $('.search .search-line').next('.filter-field').stop().slideToggle();
+    });
+});*/
+
+$(function(){
+    $('select > option').css("display", "block");
+    //$('select > option').css("color", "red");
+});

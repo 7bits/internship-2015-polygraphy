fca8e80d06f2684ffb9e0e605dc76c4ffbf5a9d0
sadullaeva <sadullaeva.marina@gmail.com>
2015-07-21 13:26:11 +0600

    Fixed a script to automatically update database

--- update-db.sh
+++ update-db.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-ssh polygraphy@192.168.200.109 -A
-"cd public/;
+ssh polygraphy@192.168.200.109 -A "cd public/;
 git pull origin develop;
 cp -Rf data /home/polygraphy;
 cd ..;
-psql -U polygraphy < data/delete_db.sql;
-psql -U polygraphy < data/polygraphy_db.sql;
-psql -U polygraphy < data/fill_db.sql"
+PGPASSWORD=polygraphy psql -U polygraphy < data/delete_db.sql;
+PGPASSWORD=polygraphy psql -U polygraphy < data/polygraphy_db.sql;
+PGPASSWORD=polygraphy psql -U polygraphy < data/fill_db.sql"

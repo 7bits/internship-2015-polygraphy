0fbeb7c27f6c109d59d7ddbeec6833a879e534c8
lyubich <anastasia.lyubich@gmail.ru>
2015-07-22 16:32:35 +0600

    Added migrations

--- src/main/java/it/sevenbits/graphicartsindustry/config/DatabaseConfig.java
+++ src/main/java/it/sevenbits/graphicartsindustry/config/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package it.sevenbits.graphicartsindustry.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.flywaydb.core.Flyway;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,13 @@ public class DatabaseConfig {
 
     @Autowired
     private DataSource dataSource;
-//
-//    @Bean(initMethod = "migrate")
-//    public Flyway flyway() {
-//        Flyway flyway = new Flyway();
-//        flyway.setDataSource(dataSource);
-//        return flyway;
-//    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource);
+        return flyway;
+    }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {

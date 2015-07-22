561fee2542a8d701fbcc3cba934c082763256be5
sadullaeva <sadullaeva.marina@gmail.com>
2015-07-22 15:40:33 +0600

    Added smooth hide search.

--- src/main/resources/public/scripts/app.js
+++ src/main/resources/public/scripts/app.js
@@ -19,3 +19,31 @@ $(function(){
     $('select > option').css("display", "block");
     //$('select > option').css("color", "red");
 });
+
+$(document).ready(function() {
+    $('form').submit(function(){
+    // собираешь параметры с формы
+    // отправляешь запрос с параметрами
+    // рисуешь резльтат
+        $.ajax({
+            url: '/results',
+            type: 'POST',
+            dataType: 'html',
+            //data: '.results',
+            success: function(data){
+                $('.results').html(data)
+            }
+        });
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    $("button.submit").click(function(){
+        $(".search").slideUp("slow")(function(){
+            $(".logo").click(function(){
+                $(".search").slideDown("slow");
+            });
+        });
+    });
+});

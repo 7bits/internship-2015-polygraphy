e7b8e74fe85b3f626784dbebb4b52a461d961988
lyubich <anastasia.lyubich@gmail.ru>
2015-07-22 14:18:16 +0600

    Changed SeachController - added function results

--- src/main/java/it/sevenbits/graphicartsindustry/web/controllers/SearchController.java
+++ src/main/java/it/sevenbits/graphicartsindustry/web/controllers/SearchController.java
@@ -7,6 +7,7 @@ import it.sevenbits.graphicartsindustry.web.service.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -61,7 +62,27 @@ public class SearchController {
         // Добавим в модель объект - список методов доставки
         model.addAttribute("deliveryMethods", service.findDeliveryMethods());
 
+        if (form.getQuery().isEmpty() && form.getServiceId()==0 && form.getPaymentMethod()==0 &&
+                form.getWritesTheCheck()==false && form.getDeliveryMethod()==0 &&
+                form.getOrderByEmail()==false)
+            // В модель добавим объект - рандомный список полиграфий
+            model.addAttribute("polygraphies", service.findAll(limitPolygraphy));
+        else {
+            List<PolygraphyMinModel> polygraphies = service.findPolygraphies(form);
+            // В модель добавим объект - список полиграфий удовлетвояющих поиску
+            model.addAttribute("polygraphies", polygraphies);
+            // Добавим в модель объект - строка, которая говорит о том, была ли найдена хоть одна полиграфия
+            if (polygraphies.size() == 0)
+                model.addAttribute("polygraphyiesIsNull", "Ни одна из полиграфий не удовлетворяет " +
+                        "требованиям запроса");
+            else
+                model.addAttribute("polygraphyiesIsNull", "");
+        }
+        return "home/index";
+    }
 
+    @RequestMapping(value = "/results", method = RequestMethod.POST)
+    public String results (@ModelAttribute SearchForm form, final Model model) throws ServiceException {
 
         if (form.getQuery().isEmpty() && form.getServiceId()==0 && form.getPaymentMethod()==0 &&
                 form.getWritesTheCheck()==false && form.getDeliveryMethod()==0 &&
@@ -80,5 +101,6 @@ public class SearchController {
                 model.addAttribute("polygraphyiesIsNull", "");
         }
         return "home/index";
+
     }
 }

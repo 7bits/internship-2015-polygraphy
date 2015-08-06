5f4235000c213f6449250939a35f58a6f9d1b130
sadullaeva <sadullaeva.marina@gmail.com>
2015-08-07 02:52:10 +0600

    Changed design to administrator's page and not only

--- src/main/resources/public/scripts/admin.js
+++ src/main/resources/public/scripts/admin.js
@@ -1,6 +1,24 @@
-/*$(document).ready(function(){
-    $('.generate-link').on("click", function(){
-        $('.generate-link').hide();
-        $('.link').show();
+
+var requestId = function(event){
+    event.preventDefault();
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    var headers = {};
+    headers[header] = token;
+    var id = $(this).attr('id');
+    $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: '/admin/registration-link',
+            headers: headers,
+            success: function(id) {
+                return id;
+            }
     });
-});*/
+}
+
+$(document).ready(function(){
+
+    $('.admin-form').on('click', 'td.col-link a', requestId);
+
+});

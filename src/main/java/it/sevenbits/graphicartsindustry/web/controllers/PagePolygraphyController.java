79290bcd616d3684023345169edda3d388d6236b
lyubich <anastasia.lyubich@gmail.com>
2015-08-06 16:48:45 +0600

    Added info in polygraphy page

--- src/main/java/it/sevenbits/graphicartsindustry/web/controllers/PagePolygraphyController.java
+++ src/main/java/it/sevenbits/graphicartsindustry/web/controllers/PagePolygraphyController.java
@@ -6,9 +6,7 @@ import it.sevenbits.graphicartsindustry.web.service.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PagePolygraphyController {
@@ -16,8 +14,16 @@ public class PagePolygraphyController {
     @Autowired
     private ContentService contentService;
 
-    @RequestMapping(value = "/polygraphy/{id:\\d+}", method = RequestMethod.GET)
-    public PolygraphyFullModel pageInfoAboutPolygraphy(@PathVariable(value = "id") int id, final Model model) throws ServiceException {
+    @RequestMapping(value = "/polygraphy/{id:\\d+}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public PolygraphyFullModel pageInfoAboutPolygraphyJson(@PathVariable(value = "id") int id, final Model model) throws ServiceException {
         return contentService.findPolygraphy(id);
+    }
+
+    @RequestMapping(value = "/polygraphy/{id:\\d+}", method = RequestMethod.GET)
+    public String pageInfoAboutPolygraphy(@PathVariable(value = "id") int id, final Model model) throws ServiceException {
+
+        model.addAttribute("polygraphy", contentService.findPolygraphy(id));
+        return "home/page_about_polygraphy";
     }
 }

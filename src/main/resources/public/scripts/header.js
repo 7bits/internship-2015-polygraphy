f8645ac6ae5ae040b5398cc69039507b055239d9
sadullaeva <sadullaeva.marina@gmail.com>
2015-08-17 16:42:37 +0600

    Fixed adaptive design for header

--- src/main/resources/public/scripts/header.js
+++ src/main/resources/public/scripts/header.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
-    $("li.list").mouseenter(function(){
+    $("li.list").click(function(){
         $(".list-item").css('visibility','visible');
     });
 
-    $("li.list").mouseleave(function(){
-            $(".list-item").css('visibility','hidden');
-        });
+    $(document).mouseup(function (e){
+        var div = $(".list-item");
+        if (!div.is(e.target)                           // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) {        // и не по его дочерним элементам
+        div.css('visibility','hidden');                 // скрываем его
+        }
+    });
 
 });

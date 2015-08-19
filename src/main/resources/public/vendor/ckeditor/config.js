5b33190679e7b2b8e3179c8cdee46e644d348b6c
Prokhorova <nastyaproh21@gmail.com>
2015-08-19 10:17:44 +0600

    Fixed design field editor

--- src/main/resources/public/vendor/ckeditor/config.js
+++ src/main/resources/public/vendor/ckeditor/config.js
@@ -5,13 +5,14 @@
 
 CKEDITOR.editorConfig = function( config )
 {
-   // config.language = 'fr';
     config.uiColor = '#64be5f';
+    config.resize_enabled = false;
+    config.removePlugins = 'elementspath';
+   //config.entities = false; //кавычки
 
     config.toolbarGroups = [
 		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
 		{ name: 'links', groups: [ 'links' ] },
@@ -20,3 +21,10 @@ CKEDITOR.editorConfig = function( config )
 
 	config.removeButtons = 'Source,Save,Preview,NewPage,Print,Templates,Cut,Copy,PasteText,Paste,Find,Replace,SelectAll,Checkbox,Form,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Strike,RemoveFormat,Blockquote,CreateDiv,BidiLtr,BidiRtl,Unlink,Anchor,Flash,Image,Table,Smiley,PageBreak,Iframe,BGColor,TextColor,Maximize,ShowBlocks,About,PasteFromWord,Language,Subscript,Superscript';
 };
+CKEDITOR.on( 'dialogDefinition', function( ev ){
+	var dialogName = ev.data.name;
+	var dialogDefinition = ev.data.definition;
+	if ( dialogName == 'link' ){dialogDefinition.removeContents( 'advanced' );dialogDefinition.removeContents( 'target' );}
+	if ( dialogName == 'image' ){dialogDefinition.removeContents( 'advanced' );dialogDefinition.removeContents( 'Link' );}
+	if ( dialogName == 'flash' ){dialogDefinition.removeContents( 'advanced' );}
+  });

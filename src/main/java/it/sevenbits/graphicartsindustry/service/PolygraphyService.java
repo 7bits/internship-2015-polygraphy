336ca653a422db98c67eb181626f602629bcac4d
lyubich <anastasia.lyubich@gmail.com>
2015-09-21 14:10:13 +0600

    Added transactions

--- src/main/java/it/sevenbits/graphicartsindustry/service/PolygraphyService.java
+++ src/main/java/it/sevenbits/graphicartsindustry/service/PolygraphyService.java
@@ -10,14 +10,20 @@ import it.sevenbits.graphicartsindustry.web.domain.polygraphy.PolygraphyFullMode
 import it.sevenbits.graphicartsindustry.web.domain.polygraphy.PolygraphyMinModel;
 import it.sevenbits.graphicartsindustry.web.forms.SearchForm;
 import it.sevenbits.graphicartsindustry.web.utils.SearchPolygraphyResolver;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PolygraphyService {
+    private static final Logger LOG = Logger.getLogger(PolygraphyService.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -33,6 +39,23 @@ public class PolygraphyService {
 
     @Autowired
     private SearchPolygraphyResolver searchPolygraphyResolver;
+
+    private static final String TX_NAME = "txService";
+    /**
+     * Spring Transaction Manager
+     */
+    @Autowired
+    private PlatformTransactionManager txManager;
+    /**
+     * Transaction settings object
+     */
+    private DefaultTransactionDefinition customTx;
+
+    public PolygraphyService() {
+        this.customTx = new DefaultTransactionDefinition();
+        this.customTx.setName(TX_NAME);
+        this.customTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+    }
 
     public List<PolygraphyAdminModel> findAllPolygraphies() throws ServiceException {
         try {
@@ -87,7 +110,9 @@ public class PolygraphyService {
     }
 
     public void removePolygraphy(int polygraphyId) throws ServiceException {
+        TransactionStatus status = null;
         try {
+            status = txManager.getTransaction(customTx);
             polygraphyServicesRepository.removePolygraphyPaymentMethods(polygraphyId);
             polygraphyServicesRepository.removePolygraphyDeliveryMethods(polygraphyId);
             polygraphyServicesRepository.removePolygraphyServices(polygraphyId);
@@ -100,7 +125,12 @@ public class PolygraphyService {
 
             if (userId != null)
                 userRepository.removeUser(userId);
+            txManager.commit(status);
         } catch (Exception e) {
+            if (status != null) {
+                txManager.rollback(status);
+                LOG.info("Rollback done.");
+            }
             throw new ServiceException("Can not remove all polygraphies. ");
         }
     }

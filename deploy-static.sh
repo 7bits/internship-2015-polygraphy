e46d375d7fcf0abb901f090fe0e665b5b2f1814b
sadullaeva <sadullaeva.marina@gmail.com>
2015-09-23 19:28:40 +0600

    Fixed deploy.sh and deploy-static.sh

--- deploy-static.sh
+++ deploy-static.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-scp -rf src/main/resources/public polygraphy@192.168.200.109:/home/polygraphy
+ssh polygraphy@192.168.200.109 -A "rm -R /home/polygraphy/public; exit"
+scp -r src/main/resources/public polygraphy@192.168.200.109:/home/polygraphy
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-scp -rf src/main/resources/public polygraphy@192.168.200.109:/home/polygraphy
 mvn clean -Dmaven.test.skip=true -Dmaven.tomcat.url=http://192.168.200.109:8080/manager/text tomcat7:redeploy

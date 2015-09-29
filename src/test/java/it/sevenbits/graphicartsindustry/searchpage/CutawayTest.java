df702b3689db335d76381d570aa0588f6a4fd881
Prokhorova <nastyaproh21@gmail.com>
2015-09-29 12:43:11 +0600

    Fixed test

--- src/test/java/it/sevenbits/graphicartsindustry/searchpage/CutawayTest.java
+++ src/test/java/it/sevenbits/graphicartsindustry/searchpage/CutawayTest.java
@@ -1,24 +1,16 @@
 //// все тесты, связанные с чекбоксом "изготовление визиток"
 //package it.sevenbits.graphicartsindustry.searchpage;
 //
-//import java.net.URL;
-//import java.sql.DriverManager;
-//import java.util.regex.Pattern;
-//import java.util.concurrent.TimeUnit;
-//
-//import com.thoughtworks.selenium.webdriven.commands.WaitForCondition;
-//import org.junit.*;
-//import static org.junit.Assert.*;
-//import static org.hamcrest.CoreMatchers.*;
-//import org.openqa.selenium.*;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.junit.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.remote.DesiredCapabilities;
 //import org.openqa.selenium.remote.RemoteWebDriver;
-//import org.openqa.selenium.support.ui.ExpectedCondition;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.Select;
-//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//import java.net.URL;
+//import java.util.concurrent.TimeUnit;
 //
 //public class CutawayTest {
 //  private boolean acceptNextAlert = true;

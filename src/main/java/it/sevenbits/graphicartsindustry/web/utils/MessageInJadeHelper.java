3aa43afa57029623a2f3817ba6b22756ba4a1b11
lyubich <anastasia.lyubich@gmail.com>
2015-10-01 12:43:15 +0600

    Added i18n in pages success

--- src/main/java/it/sevenbits/graphicartsindustry/web/utils/MessageInJadeHelper.java
+++ src/main/java/it/sevenbits/graphicartsindustry/web/utils/MessageInJadeHelper.java
@@ -7,7 +7,7 @@ import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.Locale;
 
-@JadeHelper("message")
+@JadeHelper("jadeMessage")
 public class MessageInJadeHelper {
 
     @Autowired

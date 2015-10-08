8387a60a7ee45269e5aad19c176f31279165b55d
lyubich <anastasia.lyubich@gmail.com>
2015-10-08 11:13:12 +0600

    Added validation on login form

--- src/main/java/it/sevenbits/graphicartsindustry/web/controllers/SessionController.java
+++ src/main/java/it/sevenbits/graphicartsindustry/web/controllers/SessionController.java
@@ -1,24 +1,32 @@
 package it.sevenbits.graphicartsindustry.web.controllers;
 
+import it.sevenbits.graphicartsindustry.service.MessageByLocaleService;
 import it.sevenbits.graphicartsindustry.web.utils.UserResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SessionController {
 
     @Autowired
+    private MessageByLocaleService messageByLocaleService;
+
+    @Autowired
     private UserResolver userResolver;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String getLogin(HttpServletRequest request) {
+    public String getLogin(@RequestParam(value = "error", required = false) final String error, final Model model) {
         try {
-            if (userResolver.isUserInRole("ROLE_ANONYMOUS"))
+            if (userResolver.isUserInRole("ROLE_ANONYMOUS")) {
+                if (error != null) {
+                    model.addAttribute("error", messageByLocaleService.getMessage("error.login.validate"));
+                }
                 return "session/login";
+            }
             else
                 throw new NotFoundException();
         } catch (NotFoundException e) {
